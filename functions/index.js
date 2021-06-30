880f538660cfb80bacea9ac4a498a9c6669f43bc
amine187 <amin.rhouma@gmail.com>
2021-06-30 18:34:36 +0200

    add timestamp endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+app.get('/timestamp', (request, response) => {
+  response.set('Cache-Control', 'public, may-age=300, s-maxage=600');
+  response.send(`${Date.now()}`);
+});
+
+exports.app = functions.https.onRequest(app);
